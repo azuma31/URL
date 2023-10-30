a6697e5174599d0bb64bcb01fd85447528549e2f
azuma31 <141985345+azuma31@users.noreply.github.com>
2023-10-30 16:27:57 +0900

    Update view.php

--- view.php
+++ view.php
@@ -1,1 +1,22 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // POSTリクエストからURLを取得
+    $url = $_POST['url'];
 
+    // cURLを使用して外部ウェブサイトのコンテンツを取得
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+
+    // cURLリクエストを閉じる
+    curl_close($ch);
+
+    if ($result) {
+        // 取得したコンテンツを表示
+        echo $result;
+    } else {
+        echo "URLの取得に失敗しました。";
+    }
+}
+?>
